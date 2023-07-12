8f5e3668bc4797e51b23f363c77cefd1d77dfc34
JhihYang Wu <jhihyang_wu@outlook.com>
2023-07-12 08:01:47 -0700

    Create header for 02_tiny_nerf.

---
+++ 02_tiny_nerf/main.py
@@ -0,0 +1,20 @@
+"""Script for demonstrating NeRF (Neural Radiance Fields) with a few features
+removed.
+
+This script is based off of tiny_nerf.ipynb from https://github.com/bmild/nerf
+Just like the notebook, this version of NeRF will also use 3D input instead of
+the 5D input from the complete version of NeRF. Colors and textures will look
+about the same from different camera viewing angles. There is also no
+hierarchical sampling so scenes will take longer to render.
+
+Example usage:
+Download tiny_nerf_data.npz from http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/tiny_nerf_data.npz
+python3 main.py
+"""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
